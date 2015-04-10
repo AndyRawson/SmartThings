27b67d97ce3c4d5036357e6dc67135b658545867
Andy Rawson <andy.rawson@gmail.com>
2015-04-10 02:26:11 -0500

    Improving flexibility and configuration

--- spark-smartthings.ino
+++ spark-smartthings.ino
@@ -1,123 +1,132 @@
-const int loopDelay = 1000; //time to wait between loop() runs in ms (1000ms = 1 second)
-
-const String device0_Name = "Device 0";
-const int device0_Pin = D0; // device0_ device pin 
-const int device0_Timeout = 120; // device1_ device timeout in seconds
-const int device0_DeviceType = 1; // 1 = Motion, 2 = door/window contact, 3 = other
-
-
-const int device1_Pin = D1;
-const int device1_Timeout = 120;
-const int device1_DeviceType = 1;
-  
-
-const int device2_Pin = D2;
-const int device2_Timeout = 1;
-const int device2_DeviceType = 1;
-
-int stVar1 = 0;
-int stVar2 = 0;
-int sparkVarTimeout = 300; // how often to update the web variables in seconds
-int sparkVarTimer = 0;
-
-const int led2 = D7; // LED to the side of the USB jack
+typedef struct {
+  char name[12]; //name of the Actuator
+  int pin; // the pin on the Spark that the device is connected to
+  int timeout; // 
+  int type; // 1 = Motion, 2 = door/window contact, 3 = other
+  int state; // the state the device is in
+  volatile int timer; //timer for motions and debouncing things volatile since it could be used in an interrupt
+} stActuator; // SmartThings Actuator device
 
 typedef struct {
-  String name;
-  int pin;
-  int timeout; 
-  int type;
-  int state;
-  volatile int timer;
-  int pinstate;
-} digitalDevice;
+  char name[12]; //name of the Sensor - limit of 12 char for Spark Variable Names
+  int pin; // the pin on the Spark that the device is connected to
+  int polling; // how often to update the web variables in seconds
+  int type; // 1 = Motion, 2 = door/window contact, 3 = other
+  int data; // data to send to SmartThings
+  int pollTimer; //timer for motions and debouncing things volatile since it could be used in an interrupt
+} stSensor; //SmartThings Sensor device
 
+// --------------------------------------------------------------------------------------
+const int actuatorCount = 3; // ** change this to the number of actuators you have configured
+//** change the following to suit what you have connected
+//                      name     pin   timeout  type     state   timer
+stActuator actuator0 {"Motion1",  D0,   120,      1,       0,      0}; 
+stActuator actuator1 {"Contact1", D1,   5,        2,       0,      0}; 
+stActuator actuator2 {"switch1",  D2,   1,        3,       0,      0};
 
+// --------------------------------------------------------------------------------------
+//Sensors
+// change the following to suit what you have connected
+const int sensorCount = 4; // ** change this to the number of sensors you have configured below
+//                      name     pin   timeout  type     state   timer
+stSensor sensor0 {"rssi", 100, 300, 1, 0};
+stSensor sensor1 {"motion1", D0, 300, 1, 0};
+stSensor sensor2 {"contact1", D1, 300, 1, 0};
+stSensor sensor3 {"switch1", D2, 300, 1, 0};
+// --------------------------------------------------------------------------------------
 
-int device0_On = 0; // Initial state for device0
-volatile int device0_Timer = 0;  // timer for device0
-void device0_Inter(void);  //Interrupt for device0
+//Other stuff
+const int loopDelay = 1000; //time to wait between loop() runs in ms (1000ms = 1 second)
+const int led2 = D7; // LED to the side of the USB jack
+stSensor sensors[sensorCount];
+stActuator actuators[actuatorCount];
 
-int device1_On = 0;
-volatile int device1_Timer = 0;
-void device1_Inter(void);
-
-int device2_On = 0;
-volatile int device2_Timer = 0;
-void device2_Inter(void);
-
+//Spark function def
+int setOn(String command);
+int setOff(String command);
+int setValue(String command);
+int setToggle(String command);
 
 void setup() {
-
-  pinMode(device0_Pin, INPUT_PULLDOWN);
-  attachInterrupt(device0_Pin, device0_Inter, RISING);
+  actuators[0] = actuator0;
+  actuators[1] = actuator1;
+  actuators[2] = actuator2;
   
-  pinMode(device1_Pin, INPUT_PULLDOWN);
-  attachInterrupt(device1_Pin, device1_Inter, RISING);
+  pinMode(actuator0.pin, INPUT_PULLDOWN);
+  attachInterrupt(actuator0.pin, D0_Inter, RISING);
   
-  pinMode(device2_Pin, INPUT_PULLDOWN);
-  attachInterrupt(device2_Pin, device2_Inter, CHANGE);
+  pinMode(actuator1.pin, INPUT_PULLDOWN);
+  attachInterrupt(actuator1.pin, D1_Inter, RISING);
+  
+  pinMode(actuator2.pin, INPUT_PULLDOWN);
+  attachInterrupt(actuator2.pin, D2_Inter, CHANGE);
   
   pinMode(led2, OUTPUT);
   
-  Spark.variable("stVar1", &stVar1, INT);
-  Spark.variable("stVar2", &stVar2, INT);
+  // setup the spark variables for SmartThings to Poll
+  Spark.variable(sensor0.name, &sensor0.data, INT);
+  Spark.variable(sensor1.name, &sensor1.data, INT);
   
-  Spark.subscribe("hook-response/ST_device", gotResponse, MY_DEVICES);
+  // setup the spark functions for commands from SmartThings
+  Spark.function("setOn", setOn);
+  Spark.function("setOff", setOff);
+  Spark.function("setValue", setValue);
+  Spark.function("setToggle", setToggle);
+  
+  // catch any response from SmartThings for the webhook
+  Spark.subscribe("hook-response/hook", gotResponse, MY_DEVICES);
   
   Serial.begin(9600);
 }
+
+void checkSensors() {
+        if (actuator0.state) {
+            actuator0.timer -= loopDelay;
+        if (actuator0.timer < 0) {
+            actuator0.state = 0;
+            Spark.publish("hook", "{ \"pin\": \"D0\", \"state\": \"off\" }", 60, PRIVATE);
+            //Spark.publish("device0_Off");
+            //Spark.publish("motion1off", "0", 60, PRIVATE); IFTTT
+            Serial.print("Motion Stopped on Device0_ Sensor");
+            digitalWrite(led2, LOW);
+        }
+    }
+    else {
+        if (actuator0.timer > 0) {
+            actuator0.state = 1;
+            Spark.publish("hook", "{ \"pin\": \"D0\", \"state\": \"on\" }", 60, PRIVATE);
+            //Spark.publish("device0_On");
+            //Spark.publish("motion1on", "1", 60, PRIVATE); //IFTTT
+            Serial.print("Motion Detected on Device0_ Sensor");
+            digitalWrite(led2, HIGH);
+            
+        }
+    }
+}
+
 
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
 // Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code.
 // Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
 void loop() {
-  String myTime = Time.timeStr();
+      
+  checkSensors(); //see if any sensor values need to be updated
 
-
-if (device0_On) {
-    device0_Timer = device0_Timer - loopDelay;
-    if (device0_Timer < 0) {
-        device0_On = 0;
-        Spark.publish("ST_device");
-        //Spark.publish("device0_Off");
-        //Spark.publish("motion1off", "0", 60, PRIVATE); IFTTT
-        Serial.print("Motion Stopped on Device0_ Sensor" + myTime);
-        digitalWrite(led2, LOW);
-    }
-}
-else {
-    if (device0_Timer > 0) {
-        device0_On = 1;
-        Spark.publish("ST_device");
-        //Spark.publish("device0_On");
-        //Spark.publish("motion1on", "1", 60, PRIVATE); //IFTTT
-        Serial.print("Motion Detected on Device0_ Sensor" + myTime);
-        digitalWrite(led2, HIGH);
-        
-    }
-}
-
-  //Serial.println("OFF");
-  //Serial.print("RSSI at ");
-  //Serial.print(myTime);
-  //Serial.print(" is: ");
-  //Serial.println(WiFi.RSSI());
-  updateVariables();
-  delay(loopDelay);               // Wait for 1 second 
+  updateVariables(); // TODO move this to checkSensors as a Sensor type
+  
+  delay(loopDelay); // Wait for 1 second 
 
 }
-
 
 void updateVariables() {
-    if (sparkVarTimer) {
-        sparkVarTimer -= loopDelay;  
+    if (sensor0.pollTimer) {
+        sensor0.pollTimer -= loopDelay;  
     }
     else {
-        sparkVarTimer = sparkVarTimeout * 1000;
-        stVar1 = WiFi.RSSI();
+        sensor0.pollTimer = sensor0.polling * 1000;
+        sensor0.data = WiFi.RSSI();
         Serial.print("RSSI: ");
-        Serial.println(stVar1);
+        Serial.println(sensor0.data);
     }
 }
 
@@ -125,27 +134,43 @@ void gotResponse(const char *name, const char *data) {
     Serial.print(data);
 }
 
+int setOn(String command) {
+    
+}
+
+int setOff(String command) {
+    
+}
+int setValue(String command) {
+    
+}
+int setToggle(String command) {
+    
+}
+
+int lightOn(String args) {
+    digitalWrite(led2, HIGH);
+    Serial.println("Switch On");
+    return 1;
+}
+
+int lightOff(String args) {
+    digitalWrite(led2, LOW);
+    Serial.println("Switch Off");
+    return 1;
+}
+
 // Interrupts
-
-void device2_Inter()
+void D0_Inter()
 {
-  if (digitalRead(device2_Pin)) {
-      Serial.print("Switch On");
-      digitalWrite(led2, HIGH);
-  }
-  else
-  {
-      Serial.print("Switch Off");
-      digitalWrite(led2, LOW);
-  }
+   actuator0.timer = actuator0.timeout * 1000; 
 }
 
-void device0_Inter()
+void D1_Inter()
 {
-   device0_Timer = device0_Timeout * 1000; 
+   actuator1.timer = actuator1.timeout * 1000; 
 }
-
-void device1_Inter()
+void D2_Inter()
 {
-   device1_Timer = device1_Timeout * 1000; 
+
 }
